5720f65744b9a9a26ca015cfbf3f8196689567c1
Meni Yakove <myakove@redhat.com>
2022-02-16 11:45:01 +0200

    Fix webhook create

--- github-webhook-server-container/webhook.py
+++ github-webhook-server-container/webhook.py
@@ -19,35 +19,37 @@ def create_webhook():
     with open("/config.yaml") as fd:
         repos = yaml.safe_load(fd)
 
-    for repo, data in repos["repositories"].items():
-        ngrok_url = None
-        webhook_ip = os.environ.get("GITHUB_WEBHOOK_IP")
-        github_repository = data["name"]
-        github_token = data["token"]
-        events = data["events"]
+    webhook_ip = os.environ.get("GITHUB_WEBHOOK_IP")
+    if webhook_ip == "ngrok":
+        driver = _get_firefox_driver()
+        driver.get("http://ngrok:4040/status")
+        ngrok_url = driver.find_element(
+            "xpath",
+            '//*[@id="app"]/div/div/div/div[1]/div[1]/ul/li[1]/div/table/tbody/tr[1]/td',
+        ).text
+        driver.close()
+        config = {"url": f"{ngrok_url}/github_webhook", "content_type": "json"}
 
-        if webhook_ip == "ngrok":
-            driver = _get_firefox_driver()
-            driver.get("http://ngrok:4040/status")
-            ngrok_url = driver.find_element(
-                "xpath",
-                '//*[@id="app"]/div/div/div/div[1]/div[1]/ul/li[1]/div/table/tbody/tr[1]/td',
-            ).text
-            driver.close()
-            config = {"url": f"{ngrok_url}/github_webhook", "content_type": "json"}
+        for repo, data in repos["repositories"].items():
+            github_repository = data["name"]
+            github_token = data["token"]
+            events = data["events"]
+            print(f"Creating webhook for {github_repository}")
             gapi = Github(login_or_token=github_token)
             repo = gapi.get_repo(github_repository)
             for _hook in repo.get_hooks():
                 if "ngrok.io" in _hook.config["url"]:
-                    print(f"Deleting existing webhook: {_hook.config['url']}")
+                    print(
+                        f"Deleting existing webhook for {github_repository}: {_hook.config['url']}"
+                    )
                     _hook.delete()
 
-        print(
-            f"Creating webhook: {ngrok_url or webhook_ip}/github_webhook for "
-            f"{github_repository} "
-            f"with events: {events}"
-        )
-        repo.create_hook("web", config, events, active=True)
+            print(
+                f"Creating webhook: {ngrok_url or webhook_ip}/github_webhook for "
+                f"{github_repository} "
+                f"with events: {events}"
+            )
+            repo.create_hook("web", config, events, active=True)
 
 
 if __name__ == "__main__":
