d20f328c8feb19fc880c7517b39545b910e7b276
Meni Yakove <myakove@gmail.com>
2023-03-23 17:43:45 +0200

    Use poetry and refactor code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-#poetry run python3 webhook_server_container/webhook.py
-poetry run python3 -m flask --app webhook_server_container/app.py run --host=0.0.0.0 --port=5000 --no-reload
+poetry run python3 webhook_server_container/app.py
