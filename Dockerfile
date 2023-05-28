024acc71f075d6ab0496a070f90ce94ec90f953d
Meni Yakove <myakove@gmail.com>
2023-05-28 12:41:57 +0300

    Remove gitlab support

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,8 @@ RUN set -x \
     && dnf clean all \
     && rm -rf /var/cache/yum
 
-COPY . /github-gitlab-webhook-server
-WORKDIR /github-gitlab-webhook-server
+COPY . /github-webhook-server
+WORKDIR /github-webhook-server
 RUN ln -s /usr/bin/python3 /usr/bin/python \
     && curl -sSL https://install.python-poetry.org | python3 - \
     && poetry --version \
--- webhook_server_container/webhook.py
+++ webhook_server_container/webhook.py
@@ -1,11 +1,7 @@
-import os
 from multiprocessing import Process
 
-import gitlab
-from constants import ALL_LABELS_DICT, STATIC_LABELS_DICT
+from constants import ALL_LABELS_DICT
 from github import Github
-from github.GithubException import UnknownObjectException
-from gitlab_api import GitLabApi
 from utils import get_github_repo_api, get_repository_from_config
 
 
@@ -39,51 +35,6 @@ def process_github_webhook(app, config, data, repository, webhook_ip):
         return
 
 
-def process_gitlab_webhook(app, config, data, repository, webhook_ip):
-    events = data.get("events", [])
-    container_gitlab_config = "/python-gitlab/python-gitlab.cfg"
-    if os.path.isfile(container_gitlab_config):
-        config_files = [container_gitlab_config]
-    else:
-        config_files = [os.path.join(os.path.expanduser("~"), "python-gitlab.cfg")]
-
-    gitlab_api = gitlab.Gitlab.from_config(config_files=config_files)
-    gitlab_api.auth()
-    try:
-        project_id = data["project_id"]
-        project = gitlab_api.projects.get(project_id)
-
-    except UnknownObjectException:
-        app.logger.info(f"Repository {repository} not found or token invalid")
-        return
-
-    try:
-        for _hook in project.hooks.list():
-            hook_exists = webhook_ip in _hook.url
-            if hook_exists:
-                app.logger.info(
-                    f"Deleting existing webhook for {repository}: {_hook.url}"
-                )
-                _hook.delete()
-
-        app.logger.info(
-            f"Creating webhook: {config['url']} for {repository} with events: {events}"
-        )
-        hook_data = {event: True for event in events}
-        hook_data["url"] = config["url"]
-        hook_data["enable_ssl_verification"] = False
-        project.hooks.create(hook_data)
-
-        for label_name, label_color in STATIC_LABELS_DICT.items():
-            label_color = f"#{label_color}"
-            GitLabApi.add_update_label(
-                project=project, label_color=label_color, label_name=label_name
-            )
-
-    except Exception:
-        return
-
-
 def create_webhook(app):
     app.logger.info("Preparing webhook configuration")
     repos = get_repository_from_config()
@@ -92,8 +43,6 @@ def create_webhook(app):
     for repo, data in repos["repositories"].items():
         webhook_ip = data["webhook_ip"]
         config = {"url": f"{webhook_ip}/webhook_server", "content_type": "json"}
-
-        _type = data["type"]
         repository = data["name"]
         _args = (
             app,
@@ -103,14 +52,8 @@ def create_webhook(app):
             webhook_ip,
         )
 
-        if _type == "github":
-            proc = Process(target=process_github_webhook, args=_args)
-            procs.append(proc)
-            proc.start()
-
-        elif _type == "gitlab":
-            proc = Process(target=process_gitlab_webhook, args=_args)
-            procs.append(proc)
-            proc.start()
+        proc = Process(target=process_github_webhook, args=_args)
+        procs.append(proc)
+        proc.start()
 
     return procs
