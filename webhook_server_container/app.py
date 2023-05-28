b80fd038adb8a2d9c81a4e9b923f5565e5209745
Meni Yakove <441263+myakove@users.noreply.github.com>
2023-05-28 15:18:33 +0300

    Remove gitlab support (#119)

--- webhook_server_container/app.py
+++ webhook_server_container/app.py
@@ -2,7 +2,6 @@ import urllib3
 from flask import Flask, Response, request
 from github_api import GitHubApi
 from github_repository_settings import set_repositories_settings
-from gitlab_api import GitLabApi
 from webhook import create_webhook
 
 
@@ -11,36 +10,18 @@ urllib3.disable_warnings()
 app = Flask("webhook_server")
 
 
-class GithubGitlabApiNotFoundError(Exception):
-    pass
-
-
-def get_api(github_event, gitlab_event, hook_data):
-    if github_event:
-        return GitHubApi(app=app, hook_data=hook_data)
-
-    elif gitlab_event:
-        return GitLabApi(app=app, hook_data=hook_data)
-
-    else:
-        raise GithubGitlabApiNotFoundError(hook_data)
-
-
 @app.route("/webhook_server", methods=["POST"])
 def process_webhook():
     try:
         hook_data = request.json
         github_event = request.headers.get("X-GitHub-Event")
-        gitlab_event = request.headers.get("X-GitLab-Event")
-        api = get_api(
-            github_event=github_event, gitlab_event=gitlab_event, hook_data=hook_data
-        )
+        api = GitHubApi(app=app, hook_data=hook_data)
 
         app.logger.info(
-            f"{api.repository_full_name} Event type: {github_event or gitlab_event} "
+            f"{api.repository_full_name} Event type: {github_event} "
             f"event ID: {request.headers.get('X-GitHub-Delivery')}"
         )
-        api.process_hook(data=hook_data if gitlab_event else github_event)
+        api.process_hook(data=github_event)
         return "process success"
     except Exception as ex:
         app.logger.error(f"Error: {ex}")
