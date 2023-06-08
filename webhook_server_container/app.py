764cfb68f305876cb26a0f57fa621bf1a9745866
Meni Yakove <myakove@gmail.com>
2023-06-08 16:00:11 +0300

    refactor code, decoraor for try:except

--- webhook_server_container/app.py
+++ webhook_server_container/app.py
@@ -1,5 +1,6 @@
 import urllib3
-from flask import Flask, Response, request
+from constants import FLASK_APP
+from flask import Response, request
 from github_api import GitHubApi
 from github_repository_settings import set_repositories_settings
 from webhook import create_webhook
@@ -7,58 +8,57 @@ from webhook import create_webhook
 
 urllib3.disable_warnings()
 
-app = Flask("webhook-server")
 PLAIN_TEXT_MIME_TYPE = "text/plain"
 APP_ROOT_PATH = "/webhook_server"
 FILENAME_STRING = "<string:filename>"
 
 
-@app.route(APP_ROOT_PATH, methods=["POST"])
+@FLASK_APP.route(APP_ROOT_PATH, methods=["POST"])
 def process_webhook():
     try:
         hook_data = request.json
         github_event = request.headers.get("X-GitHub-Event")
-        api = GitHubApi(app=app, hook_data=hook_data)
+        api = GitHubApi(hook_data=hook_data)
 
-        app.logger.info(
+        FLASK_APP.logger.info(
             f"{api.repository_full_name} Event type: {github_event} "
             f"event ID: {request.headers.get('X-GitHub-Delivery')}"
         )
         api.process_hook(data=github_event)
         return "process success"
     except Exception as ex:
-        app.logger.error(f"Error: {ex}")
+        FLASK_APP.logger.error(f"Error: {ex}")
         return "Process failed"
 
 
-@app.route(f"{APP_ROOT_PATH}/tox/{FILENAME_STRING}")
+@FLASK_APP.route(f"{APP_ROOT_PATH}/tox/{FILENAME_STRING}")
 def return_tox(filename):
-    app.logger.info("app.route: Processing tox file")
+    FLASK_APP.logger.info("app.route: Processing tox file")
     with open(f"{APP_ROOT_PATH}/tox/{filename}") as fd:
         return Response(fd.read(), mimetype=PLAIN_TEXT_MIME_TYPE)
 
 
-@app.route(f"{APP_ROOT_PATH}/build-container/{FILENAME_STRING}")
+@FLASK_APP.route(f"{APP_ROOT_PATH}/build-container/{FILENAME_STRING}")
 def return_build_container(filename):
-    app.logger.info("app.route: Processing build-container file")
+    FLASK_APP.logger.info("app.route: Processing build-container file")
     with open(f"{APP_ROOT_PATH}/build-container/{filename}") as fd:
         return Response(fd.read(), mimetype=PLAIN_TEXT_MIME_TYPE)
 
 
-@app.route(f"{APP_ROOT_PATH}/python-module-install/{FILENAME_STRING}")
+@FLASK_APP.route(f"{APP_ROOT_PATH}/python-module-install/{FILENAME_STRING}")
 def return_python_module_install(filename):
-    app.logger.info("app.route: Processing python-module-install file")
+    FLASK_APP.logger.info("app.route: Processing python-module-install file")
     with open(f"{APP_ROOT_PATH}/python-module-install/{filename}") as fd:
         return Response(fd.read(), mimetype=PLAIN_TEXT_MIME_TYPE)
 
 
 def main():
-    procs = create_webhook(app=app) + set_repositories_settings(app=app)
+    procs = create_webhook() + set_repositories_settings()
     for proc in procs:
         proc.join()
 
-    app.logger.info(f"Starting {app.name} app")
-    app.run(port=5000, host="0.0.0.0", use_reloader=False)
+    FLASK_APP.logger.info(f"Starting {FLASK_APP.name} app")
+    FLASK_APP.run(port=5000, host="0.0.0.0", use_reloader=False)
 
 
 if __name__ == "__main__":
--- webhook_server_container/webhook.py
+++ webhook_server_container/webhook.py
@@ -1,42 +1,39 @@
 from multiprocessing import Process
 
-from constants import ALL_LABELS_DICT
+from constants import ALL_LABELS_DICT, FLASK_APP
 from github import Github
-from utils import get_github_repo_api, get_repository_from_config
+from utils import get_github_repo_api, get_repository_from_config, ignore_exceptions
 
 
-def process_github_webhook(app, config, data, repository, webhook_ip):
+@ignore_exceptions()
+def process_github_webhook(config, data, repository, webhook_ip):
     token = data["token"]
     events = data.get("events", ["*"])
     gapi = Github(login_or_token=token)
-    repo = get_github_repo_api(gapi=gapi, app=app, repository=repository)
+    repo = get_github_repo_api(gapi=gapi, repository=repository)
     if not repo:
         return
 
-    try:
-        for _hook in repo.get_hooks():
-            hook_exists = webhook_ip in _hook.config["url"]
-            if hook_exists:
-                app.logger.info(
-                    f"Deleting existing webhook for {repository}: {_hook.config['url']}"
-                )
-                _hook.delete()
+    for _hook in repo.get_hooks():
+        hook_exists = webhook_ip in _hook.config["url"]
+        if hook_exists:
+            FLASK_APP.logger.info(
+                f"Deleting existing webhook for {repository}: {_hook.config['url']}"
+            )
+            _hook.delete()
 
-        app.logger.info(
-            f"Creating webhook: {config['url']} for {repository} with events: {events}"
-        )
-        repo.create_hook("web", config, events, active=True)
-        for label in repo.get_labels():
-            label_name = label.name.lower()
-            if label_name in ALL_LABELS_DICT:
-                label.edit(label.name, color=ALL_LABELS_DICT[label_name])
-
-    except Exception:
-        return
+    FLASK_APP.logger.info(
+        f"Creating webhook: {config['url']} for {repository} with events: {events}"
+    )
+    repo.create_hook("web", config, events, active=True)
+    for label in repo.get_labels():
+        label_name = label.name.lower()
+        if label_name in ALL_LABELS_DICT:
+            label.edit(label.name, color=ALL_LABELS_DICT[label_name])
 
 
-def create_webhook(app):
-    app.logger.info("Preparing webhook configuration")
+def create_webhook():
+    FLASK_APP.logger.info("Preparing webhook configuration")
     repos = get_repository_from_config()
 
     procs = []
@@ -45,7 +42,6 @@ def create_webhook(app):
         config = {"url": f"{webhook_ip}/webhook_server", "content_type": "json"}
         repository = data["name"]
         _args = (
-            app,
             config,
             data,
             repository,
