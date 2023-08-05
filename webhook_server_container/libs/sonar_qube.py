43ae8d153fd0d4c75665f4bfaaafb67c595da8e6
Meni Yakove <myakove@gmail.com>
2023-08-05 15:14:14 +0300

    Create project for the repository in sonaqube if not exists

---
+++ webhook_server_container/libs/sonar_qube.py
@@ -0,0 +1,41 @@
+import os
+
+from sonarqube import SonarQubeClient
+
+from webhook_server_container.utils.helpers import run_command
+
+
+class SonarQubeExt(SonarQubeClient):
+    def __init__(self, url, token):
+        super().__init__(sonarqube_url=url, token=token)
+        self.token = token
+
+    def get_project(self, project_key):
+        return self.projects.get_project(project_key)
+
+    def create_project(self, project_key, project_name):
+        self.projects.request(
+            method="POST",
+            path="api/projects/create",
+            params={"name": project_name, "project": project_key},
+        )
+
+    def run_sonar_scanner(self, project_key, log_prefix):
+        _cli = os.path.join(
+            os.environ.get("SONAR_SCANNER_CLI_DIR", "/sonar-scanner-cli"),
+            "bin",
+            "sonar-scanner",
+        )
+        cmd = (
+            f"{_cli} -Dsonar.projectKey={project_key} "
+            f"-Dsonar.sources=. "
+            f"-Dsonar.host.url={self.base_url} "
+            f"-Dsonar.token={self.token}"
+        )
+        return run_command(command=cmd, log_prefix=log_prefix)
+
+    def get_project_quality_status(self, project_key):
+        self.qualitygates.request(
+            path="api/qualitygates/project_status",
+            params={"projectKey": project_key},
+        ).json()
