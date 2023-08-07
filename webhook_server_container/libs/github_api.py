ea028ad5db37403dbd76d88872b1ed9d692dd06c
Meni Yakove <myakove@gmail.com>
2023-08-08 01:43:36 +0300

    Run command is container

--- webhook_server_container/libs/github_api.py
+++ webhook_server_container/libs/github_api.py
@@ -1423,7 +1423,7 @@ Adding label/s `{' '.join([_cp_label for _cp_label in cp_labels])}` for automati
         ]
         for proc in procs:
             proc.start()
-            time.sleep(10)
+            time.sleep(30)
 
         for proc in procs:
             proc.join()
