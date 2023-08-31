5a570216c81ce383c281afb029dc6f08b73804f9
Meni Yakove <myakove@gmail.com>
2023-08-31 08:32:39 +0300

    run in container use host network

--- webhook_server_container/libs/github_api.py
+++ webhook_server_container/libs/github_api.py
@@ -1549,8 +1549,9 @@ Adding label/s `{' '.join([_cp_label for _cp_label in cp_labels])}` for automati
 
     def _run_in_container(self, command, env=None, file_path=None):
         podman_base_cmd = (
-            f"podman run --privileged -v /tmp/containers:/var/lib/containers/:Z --rm {env if env else ''} "
-            f"--entrypoint bash quay.io/myakove/github-webhook-server -c"
+            "podman run --network=host --privileged -v /tmp/containers:/var/lib/containers/:Z "
+            f"--rm {env if env else ''} "
+            "--entrypoint bash quay.io/myakove/github-webhook-server -c"
         )
 
         # Clone the repository
