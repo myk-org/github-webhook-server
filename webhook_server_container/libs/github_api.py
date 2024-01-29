f38c9ae58325cacd2fc29f68d71180991e2d3487
Meni Yakove <myakove@gmail.com>
2024-01-29 10:57:53 +0200

    fail checkrun if fail to post res

--- webhook_server_container/libs/github_api.py
+++ webhook_server_container/libs/github_api.py
@@ -1275,7 +1275,11 @@ Adding label/s `{' '.join([_cp_label for _cp_label in cp_labels])}` for automati
             kwargs["output"] = output
 
         self.app.logger.info(f"{self.log_prefix} Set {check_run} check to {status or conclusion}")
-        self.repository_by_github_app.create_check_run(**kwargs)
+        try:
+            self.repository_by_github_app.create_check_run(**kwargs)
+        except Exception:
+            self.app.logger.info(f"{self.log_prefix} Failed to set {check_run} check to {status or conclusion}")
+            self.repository_by_github_app.create_check_run(conclusion=FAILURE_STR)
         return f"Done setting check run status: {kwargs}"
 
     def _run_in_container(self, command, env=None):
