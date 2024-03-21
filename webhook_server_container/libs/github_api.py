5a82fa579bb34ae4c67aa4fe808fa5289f0f6edf
Meni Yakove <myakove@gmail.com>
2024-03-21 17:09:21 +0200

    Fix or if for tag

--- webhook_server_container/libs/github_api.py
+++ webhook_server_container/libs/github_api.py
@@ -1258,11 +1258,7 @@ Adding label/s `{" ".join([_cp_label for _cp_label in cp_labels])}` for automati
         """
 
     def _container_repository_and_tag(self, tag=None):
-        tag = (
-            tag or self.container_tag
-            if self.pull_request and self.pull_request.is_merged()
-            else f"pr-{self.pull_request.number}"
-        )
+        tag = tag or (self.container_tag if self.pull_request.is_merged() else f"pr-{self.pull_request.number}")
         return f"{self.container_repository}:{tag}"
 
     def _run_build_container(self, set_check=True, push=False, is_merged=None, tag=None):
