99dde9df83a8bea821b26b9149951df4b7c4a516
rnetser <rnetser@redhat.com>
2024-06-25 15:12:04 +0300

    remove label module

--- webhook_server_container/libs/label.py
+++
@@ -1,8 +0,0 @@
-class Label:
-    def __init__(self, name, color, description):
-        self.name = name
-        self.color = color
-        self.description = description
-
-    def __str__(self):
-        return f"Label(name={self.name}, color={self.color}, description={self.description})"
