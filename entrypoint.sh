a3da1ab2222c6c07eb7f175a9515a7d152c04078
Meni Yakove <myakove@gmail.com>
2024-08-20 21:07:29 +0300

    run rm -rf /run/containers/storage /run/libpod on server start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf /run/containers/storage /run/libpod
+
 SERVER_RUN_CMD="poetry run uvicorn webhook_server_container.app:FASTAPI_APP --host 0.0.0.0 --port 5000 "
 UVICORN_WORKERS="${UVICORN_MAX_WORKERS:=10}"
 
@@ -9,7 +11,7 @@ poetry run python webhook_server_container/utils/github_repository_settings.py
 poetry run python webhook_server_container/utils/webhook.py
 
 if [[ -z $DEVELOPMENT ]]; then
-	eval "${SERVER_RUN_CMD} --workers ${UVICORN_WORKERS}"
+  eval "${SERVER_RUN_CMD} --workers ${UVICORN_WORKERS}"
 else
-	eval "${SERVER_RUN_CMD} --reload"
+  eval "${SERVER_RUN_CMD} --reload"
 fi
