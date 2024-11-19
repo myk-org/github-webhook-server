f51811f58137147b7d5cba0592aee3822fbf10f6
Meni Yakove <myakove@gmail.com>
2024-11-19 15:39:50 +0200

    Add script to extract commands per PR from the log

--- scripts/get_pr_commands_from_log.sh
+++ scripts/get_pr_commands_from_log.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Usage:./get_pr_commands_from_log.sh <log_file> <pr_number>
+
 LOG_FILE=$1
 PR_NUMBER=$2
 cat $LOG_FILE | grep "$PR_NUMBER" | grep "Running" | awk -F": Running '" '{print $2}' | awk -F"' command" '{print $1}'
