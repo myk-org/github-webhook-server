e17e9961442c531e17a61cb35a0ae7bc4bf40697
Meni Yakove <myakove@gmail.com>
2024-11-26 15:17:14 +0200

    Fix  is not mergeable fstring

--- webhook_server_container/libs/github_api.py
+++ webhook_server_container/libs/github_api.py
@@ -1299,7 +1299,7 @@ stderr: `{_err}`
             self.logger.debug(f"{self.log_prefix} check if can be merged. PR labels are: {_labels}")
 
             if not self.pull_request.mergeable:
-                failure_output += "PR is not mergeable: {self.pull_request.mergeable_state}\n"
+                failure_output += f"PR is not mergeable: {self.pull_request.mergeable_state}\n"
 
             required_check_in_progress_failure_output, check_runs_in_progress = self._required_check_in_progress(
                 last_commit_check_runs=last_commit_check_runs
