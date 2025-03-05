d836ecf17b2bfdbb584410db4995778a6b0af2de
Meni Yakove <441263+myakove@users.noreply.github.com>
2025-03-05 14:06:33 +0200

    feat: rebase pull request branch on checkout PR (#674)

--- webhook_server_container/libs/github_api.py
+++ webhook_server_container/libs/github_api.py
@@ -1642,6 +1642,7 @@ stderr: `{_err}`
             # Checkout to requested branch/tag
             if checkout:
                 run_command(f"{git_cmd} checkout {checkout}", log_prefix=self.log_prefix)
+                run_command(f"{git_cmd} rebase {self.pull_request_branch}", log_prefix=self.log_prefix)
 
             # Checkout the branch if pull request is merged or for release
             else:
@@ -1662,6 +1663,7 @@ stderr: `{_err}`
                     run_command(
                         command=f"{git_cmd} checkout origin/pr/{pull_request.number}", log_prefix=self.log_prefix
                     )
+                    run_command(f"{git_cmd} rebase {self.pull_request_branch}", log_prefix=self.log_prefix)
 
             yield
 
