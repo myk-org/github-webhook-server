27f17cd732a1bac6f54c73224a05eb45f2e39957
Meni Yakove <441263+myakove@users.noreply.github.com>
2025-03-06 17:37:11 +0200

    Use merge origin/<branch> for check run (#682)

--- webhook_server_container/libs/github_api.py
+++ webhook_server_container/libs/github_api.py
@@ -1692,7 +1692,7 @@ Publish to PYPI failed: `{_error}`
 
                 if getattr(self, "pull_request", None):
                     rc, out, err = run_command(
-                        f"{git_cmd} merge {self.pull_request_branch} -m 'Merge {self.pull_request_branch}'",
+                        f"{git_cmd} merge origin/{self.pull_request_branch} -m 'Merge {self.pull_request_branch}'",
                         log_prefix=self.log_prefix,
                     )
                     if not rc:
@@ -1724,7 +1724,7 @@ Publish to PYPI failed: `{_error}`
 
                         if getattr(self, "pull_request", None):
                             rc, out, err = run_command(
-                                f"{git_cmd} merge {self.pull_request_branch} -m 'Merge {self.pull_request_branch}'",
+                                f"{git_cmd} merge origin/{self.pull_request_branch} -m 'Merge {self.pull_request_branch}'",
                                 log_prefix=self.log_prefix,
                             )
                             if not rc:
