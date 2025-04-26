3cb26a0d9c194aee902325ed41cb0e508669a45b
Meni Yakove <myakove@gmail.com>
2025-04-26 14:33:53 +0300

    Fix MAX_WORKERS missmatch

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,7 @@ import os
 
 bind = f"{os.environ.get('WEBHOOK_SERVER_IP_BIND', '0.0.0.0')}:{os.environ.get('WEBHOOK_SERVER_PORT', 5000)}"
 worker_class = "uvicorn.workers.UvicornWorker"
-workers = int(os.environ.get("UVICORN_WORKERS", 10))
+workers = int(os.environ.get("MAX_WORKERS", 10))
 on_starting = "webhook_server.app.on_starting"
 accesslog = "-"
 timeout = 0  # Disable workers timeout, some operation can take long time.
